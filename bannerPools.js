50f01c3784a3995202a94539a8ba0e34c5518e3b
leox7-bths <leox7@nycstudents.net>
2026-03-01 14:58:56 -0500

    Add files via upload
    Reworked collection function
    Added collection.html
    Added bannerPools.js
    Added the ability to view character's 3d model

---
+++ bannerPools.js
@@ -0,0 +1,171 @@
+// BANNER POOLS
+
+// STANDARD 5 star CHARACTERS
+const standard5 = [
+    { name:"Welt", rarity:5, img:"s5c/Character_Welt_Splash_Art.webp" },
+    { name:"Gepard", rarity:5, img:"s5c/Character_Gepard_Splash_Art.webp" },
+    { name:"Himeko", rarity:5, img:"s5c/Character_Himeko_Splash_Art.webp" },
+    { name:"Yanqing", rarity:5, img:"s5c/Character_Yanqing_Splash_Art.webp" },
+    { name:"Clara", rarity:5, img:"s5c/Character_Clara_Splash_Art.webp" },
+    { name:"Bronya", rarity:5, img:"s5c/Character_Bronya_Splash_Art.webp" },
+    { name:"Bailu", rarity:5, img:"s5c/Character_Bailu_Splash_Art.webp" }
+];
+
+// STANDARD 5 star lightcone
+const standardlc5 = [
+    { name:"In the Name of the World", rarity:5, img:"s5lc/Light_Cone_In_the_Name_of_the_World.webp" },
+    { name:"Moment of Victory", rarity:5, img:"s5lc/Light_Cone_Moment_of_Victory.webp" },
+    { name:"Night on the Milky Way", rarity:5, img:"s5lc/Light_Cone_Night_on_the_Milky_Way.webp" },
+    { name:"Sleep Like the Dead", rarity:5, img:"s5lc/Light_Cone_Sleep_Like_the_Dead.webp" },
+    { name:"Something Irreplaceable", rarity:5, img:"s5lc/Light_Cone_Something_Irreplaceable.webp" },
+    { name:"But the Battle Isn't Over", rarity:5, img:"s5lc/Light_Cone_But_the_Battle_Isnt_Over.webp" },
+    { name:"Time Waits for No One", rarity:5, img:"s5lc/Light_Cone_Time_Waits_for_No_One.webp" }
+];
+
+// STANDARD 4 star CHARACTERS
+const all4 = [
+    { name:"Asta", rarity:4, img:"s4c/Character_Asta_Splash_Art.webp" },
+    { name:"Serval", rarity:4, img:"s4c/Character_Serval_Splash_Art.webp" },
+    { name:"Natasha", rarity:4, img:"s4c/Character_Natasha_Splash_Art.webp" },
+    { name:"Hook", rarity:4, img:"s4c/Character_Hook_Splash_Art.webp" },
+    { name:"Pela", rarity:4, img:"s4c/Character_Pela_Splash_Art.webp" },
+    { name:"March 7th", rarity:4, img:"s4c/Character_March_7th_29_Splash_Art.webp" },
+    { name:"Tingyun", rarity:4, img:"s4c/Character_Tingyun_Splash_Art.webp" },
+    { name:"Sushang", rarity:4, img:"s4c/Character_Sushang_Splash_Art.webp" },
+    { name: "Sampo", rarity: 4, img: "s4c/Character_Sampo_Splash_Art.webp"},
+    { name: "Qingque", rarity: 4, img: "s4c/Character_Qingque_Splash_Art.webp"},
+    { name: "Dan Heng", rarity: 4, img: "s4c/Character_Dan_Heng_Splash_Art.webp"}
+];
+
+// 3 star LIGHT CONES
+const lightcones3 = [
+    { name:"Fine Fruit", rarity:3, img:"3lc/Light_Cone_Fine_Fruit.webp" },
+    { name:"Passkey", rarity:3, img:"3lc/Light_Cone_Passkey.webp" },
+    { name:"Amber", rarity:3, img:"3lc/Light_Cone_Amber.webp" },
+    { name:"Void", rarity:3, img:"3lc/Light_Cone_Void.webp" },
+    { name:"Chorus", rarity:3, img:"3lc/Light_Cone_Chorus.webp" },
+    { name:"Data Bank", rarity:3, img:"3lc/Light_Cone_Data_Bank.webp" },
+    { name:"Darting Arrow", rarity:3, img:"3lc/Light_Cone_Darting_Arrow.webp" },
+    { name:"Collapsing Sky", rarity:3, img:"3lc/Light_Cone_Collapsing_Sky.webp" },
+    { name:"Shattered Home", rarity:3, img:"3lc/Light_Cone_Shattered_Home.webp" },
+    { name:"Loop", rarity:3, img:"3lc/Light_Cone_Loop.webp" },
+    { name:"Meshing Cogs", rarity:3, img:"3lc/Light_Cone_Meshing_Cogs.webp" },
+    { name:"Defense", rarity:3, img:"3lc/Light_Cone_Defense.webp" },
+    { name:"Mutual Demise", rarity:3, img:"3lc/Light_Cone_Mutual_Demise.webp" },
+    { name:"Sneering", rarity:3, img:"3lc/Light_Cone_Sneering.webp" },
+    { name:"Shadowburn", rarity:3, img:"3lc/Light_Cone_Shadowburn.webp" },
+    { name:"Sagacity", rarity:3, img:"3lc/Light_Cone_Sagacity.webp" },
+    { name:"Reminiscence", rarity:3, img:"3lc/Light_Cone_Reminiscence.webp" },
+    { name:"Pioneering", rarity:3, img:"3lc/Light_Cone_Pioneering.webp" },
+    { name:"Multiplication", rarity:3, img:"3lc/Light_Cone_Multiplication.webp" },
+    { name:"Mediation", rarity:3, img:"3lc/Light_Cone_Mediation.webp" },
+    { name:"Lingering Tear", rarity:3, img:"3lc/Light_Cone_Lingering_Tear.webp" },
+    { name:"Hidden Shadow", rarity:3, img:"3lc/Light_Cone_Hidden_Shadow.webp" },
+    { name:"Cornucopia", rarity:3, img:"3lc/Light_Cone_Cornucopia.webp" },
+    { name:"Arrows", rarity:3, img:"3lc/Light_Cone_Arrows.webp" },
+    { name:"Adversarial", rarity:3, img:"3lc/Light_Cone_Adversarial.webp" }
+];
+
+const lightcones4 = [
+    { name:"Only Silence Remains", rarity:4, img:"s4lc/Light_Cone_Only_Silence_Remains.webp" },
+    { name:"Swordplay", rarity:4, img:"s4lc/Light_Cone_Swordplay.webp" },
+    { name:"River Flows in Spring", rarity:4, img:"s4lc/Light_Cone_River_Flows_in_Spring.webp" },
+    { name:"For Tomorrow's Journey", rarity:4, img:"s4lc/Light_Cone_For_Tomorrows_Journey.webp" },
+    { name:"Subscribe for More!", rarity:4, img:"s4lc/Light_Cone_Subscribe_for_More.webp" },
+    { name:"Geniuses' Repose", rarity:4, img:"s4lc/Light_Cone_Geniuses_Repose.webp" },
+    { name:"Memories of the Past", rarity:4, img:"s4lc/Light_Cone_Memories_of_the_Past.webp" },
+    { name:"The Moles Welcome You", rarity:4, img:"s4lc/Light_Cone_The_Moles_Welcome_You.webp" },
+    { name:"Planetary Rendezvous", rarity:4, img:"s4lc/Light_Cone_Planetary_Rendezvous.webp" },
+    { name:"Dance! Dance! Dance!", rarity:4, img:"s4lc/Light_Cone_Dance21_Dance.webp" },
+    { name:"Carve the Moon, Weave the Clouds", rarity:4, img:"s4lc/Light_Cone_Carve_the_Moon_Weave_the_Clouds.webp" },
+    { name:"Shared Feeling", rarity:4, img:"s4lc/Light_Cone_Shared_Feeling.webp" },
+    { name:"Post-Op Conversation", rarity:4, img:"s4lc/Light_Cone_Post-Op_Conversation.webp" },
+    { name:"Perfect Timing", rarity:4, img:"s4lc/Light_Cone_Perfect_Timing.webp" },
+    { name:"Quid Pro Quo", rarity:4, img:"s4lc/Light_Cone_Quid_Pro_Quo.webp" },
+    { name:"Trend of the Universal Market", rarity:4, img:"s4lc/Light_Cone_Trend_of_the_Universal_Market.webp" },
+    { name:"Landau's Choice", rarity:4, img:"s4lc/Light_Cone_Landaus_Choice.webp" },
+    { name:"Day One of My New Life", rarity:4, img:"s4lc/Light_Cone_Day_One_of_My_New_Life.webp" },
+    { name:"This Is Me!", rarity:4, img:"s4lc/Light_Cone_This_Is_Me.webp" },
+    { name:"Good Night and Sleep Well", rarity:4, img:"s4lc/Light_Cone_Good_Night_and_Sleep_Well.webp" },
+    { name:"We Are Wildfire", rarity:4, img:"s4lc/Light_Cone_We_Are_Wildfire.webp" },
+    { name: "Make the World Clamor", rarity: 4, img: "s4lc/Light_Cone_Make_the_World_Clamor.webp"}
+];
+
+// const limited5 = [
+//     { name:"Acheron", rarity:5, img:"https://static.wikia.nocookie.net/houkai-star-rail/images/7/78/Character_Acheron_Splash_Art.png" },
+//     { name:"Kafka", rarity:5, img:"https://static.wikia.nocookie.net/houkai-star-rail/images/9/95/Character_Kafka_Splash_Art.png" }
+// ];
+
+// Seele
+const seeleLimited5 = [
+    { name: "Seele", rarity: 5, img: "l5c/Character_Seele_Splash_Art.webp" }
+];
+
+const seeleLimited4 = [
+    { name: "Natasha", rarity: 4, img: "s4c/Character_Natasha_Splash_Art.webp", rate: 0.33 },
+    { name: "Hook", rarity: 4, img: "s4c/Character_Hook_Splash_Art.webp", rate: 0.33 },
+    { name: "Pela", rarity: 4, img: "s4c/Character_Pela_Splash_Art.webp", rate: 0.33 }
+];
+
+const seelelcLimited5 = [
+    { name: "In the Night", rarity: 5, img: "l5lc/Light_Cone_In_the_Night.webp" }
+];
+
+const seelelcLimited4 = [
+    { name: "The Moles Welcome You", rarity: 4, img: "s4lc/Light_Cone_The_Moles_Welcome_You.webp", rate: 0.33 },
+    { name: "Good Night and Sleep Well", rarity: 4, img: "s4lc/Light_Cone_Good_Night_and_Sleep_Well.webp", rate: 0.33 },
+    { name: "Post-Op Conversation", rarity: 4, img: "s4lc/Light_Cone_Post-Op_Conversation.webp", rate: 0.33 }
+];
+
+// Jing yuan
+const jingyuanLimited5 = [
+    { name: "Jing Yuan", rarity: 5, img: "l5c/Character_Jing_Yuan_Splash_Art.webp" }
+];
+
+const jingyuanLimited4 = [
+    { name: "March 7th", rarity: 4, img: "s4c/Character_March_7th_29_Splash_Art.webp", rate: 0.33 },
+    { name: "Tingyun", rarity: 4, img: "s4c/Character_Tingyun_Splash_Art.webp", rate: 0.33 },
+    { name: "Sushang", rarity: 4, img: "s4c/Character_Sushang_Splash_Art.webp", rate: 0.33 }
+];
+
+const jingyuanlcLimited5 = [
+    { name: "Before Dawn", rarity: 5, img: "l5lc/Light_Cone_Before_Dawn.webp" }
+];
+
+const jingyuanlcLimited4 = [
+    { name:"Day One of My New Life", rarity:4, img:"s4lc/Light_Cone_Day_One_of_My_New_Life.webp", rate: 0.33 },
+    { name:"Only Silence Remains", rarity:4, img:"s4lc/Light_Cone_Only_Silence_Remains.webp", rate: 0.33 },
+    { name:"Planetary Rendezvous", rarity:4, img:"s4lc/Light_Cone_Planetary_Rendezvous.webp", rate: 0.33 }
+];
+
+// Silver Wolf
+const silverWolfLimited5 = [
+    { name: "Silver Wolf", rarity: 5, img: "l5c/Character_Silver_Wolf_Splash_Art.webp" }
+];
+
+const silverWolfLimited4 = [
+    { name: "Sampo", rarity: 4, img: "s4c/Character_Sampo_Splash_Art.webp", rate: 0.33 },
+    { name: "Qingque", rarity: 4, img: "s4c/Character_Qingque_Splash_Art.webp", rate: 0.33 },
+    { name: "Dan Heng", rarity: 4, img: "s4c/Character_Dan_Heng_Splash_Art.webp", rate: 0.33 }
+];
+
+const silverWolflcLimited5 = [
+    { name: "Incessant Rain", rarity: 5, img: "l5lc/Light_Cone_Incessant_Rain.webp" }
+];
+
+const silverWolflcLimited4 = [
+    { name:"Subscribe for More!", rarity:4, img:"s4lc/Light_Cone_Subscribe_for_More.webp", rate: 0.33 },
+    { name:"Memories of the Past", rarity:4, img:"s4lc/Light_Cone_Memories_of_the_Past.webp", rate: 0.33 },
+    { name: "Make the World Clamor", rarity: 4, img: "s4lc/Light_Cone_Make_the_World_Clamor.webp", rate: 0.33 }
+];
+
+//BANNER IMAGE
+const bannerImages = {
+    standard: "images/Stellar_Warp.webp",
+    seele: "images/Butterfly_on_Swordtip_2023-04-26.webp",
+    seelelc: "images/Brilliant_Fixation_2023-04-26.webp",
+    jingyuan: "images/Swirl_of_Heavenly_Spear_2023-05-17.webp",
+    jingyuanlc: "images/Brilliant_Fixation_2023-05-17.webp",
+    silverwolf: "images/Contract_Zero_2025-09-02.webp",
+    silverwolflc: "images/Brilliant_Fixation_2023-06-07.webp"
+};
